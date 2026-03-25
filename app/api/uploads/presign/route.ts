3f91dd635310f1ce16792d1e55a3512293189cb9
shine <clangod002@gmail.com>
2026-03-25 15:06:55 +0700

    Updat presign

--- app/api/uploads/presign/route.ts
+++ app/api/uploads/presign/route.ts
@@ -5,8 +5,10 @@ const ACCOUNT_ID = 'b2beffacb9f79d59a56348879857cead'
 const ACCESS_KEY_ID = 'f4112e555214f3b4781889ffca1fd4da'
 const SECRET_ACCESS_KEY = '61dc280efe9f771e75ab95fced9febab6d50aef27b6768241dab9ebb5acbaa69'
 const BUCKET_NAME = 'public-game'
-const endpoint = "https://b2beffacb9f79d59a56348879857cead.r2.cloudflarestorage.com"
+const endpoint = "https://b2beffacb9f79d59a56348879857cead.r2.cloudflarestorage.com/public-game"
 // endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
+
+
 
 const s3 = new S3Client({
     region: 'auto',
